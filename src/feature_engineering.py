0d61d0f1caa1f1337146dcd578057bb007dc073e
Islam Nabi <islamnabics@gmail.com>
2025-05-25 23:15:11 +0500

    Pipeline Configured Using Params

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -26,7 +26,6 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
-"""
 def load_params(params_path: str) -> dict:
     # Load parameters from a YAML file.
     try:
@@ -44,7 +43,6 @@ def load_params(params_path: str) -> dict:
         logger.error('Unexpected error: %s', e)
         raise
         
-"""
 
 def load_data(file_path: str) -> pd.DataFrame:
     """Load data from a CSV file."""
@@ -97,9 +95,9 @@ def save_data(df: pd.DataFrame, file_path: str) -> None:
 
 def main():
     try:
-        #params = load_params(params_path='params.yaml')
-        #max_features = params['feature_engineering']['max_features']
-        max_features = 50
+        params = load_params(params_path='params.yaml')
+        max_features = params['feature_engineering']['max_features']
+        #max_features = 50
 
         train_data = load_data('./data/interim/train_processed.csv')
         test_data = load_data('./data/interim/test_processed.csv')
--- src/model_building.py
+++ src/model_building.py
@@ -28,7 +28,6 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
-"""
 def load_params(params_path: str) -> dict:
     # Load parameters from a YAML file.
     try:
@@ -45,7 +44,6 @@ def load_params(params_path: str) -> dict:
     except Exception as e:
         logger.error('Unexpected error: %s', e)
         raise
-"""
 
 def load_data(file_path: str) -> pd.DataFrame:
     """
@@ -120,7 +118,7 @@ def save_model(model, file_path: str) -> None:
 
 def main():
     try:
-        params = {'n_estimators': 25, 'random_state': 2}
+        params = load_params('params.yaml')['model_building']
         train_data = load_data('./data/processed/train_tfidf.csv')
         X_train = train_data.iloc[:, :-1].values
         y_train = train_data.iloc[:, -1].values
